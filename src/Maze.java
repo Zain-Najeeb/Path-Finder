372ba9310a4d1f220252800ea3a593b16770ed4c
Zain-Najeeb <m.zainnajeeb@gmail.com>
2022-08-21 20:03:10 -0500

    quality of life + features
    Added a score/ability to use username (will be adding database to save these soon). and able to generate new mazes

--- src/Maze.java
+++ src/Maze.java
@@ -1,31 +1,49 @@
+
+
 import java.awt.geom.Point2D;
 import processing.core.PApplet;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class Maze extends PApplet{
+public class Maze extends PApplet {
+    public static int score = 0;
+    public static int highscore = 0 ;
+
+    public static player player = new player();
+    public static Maze maze = new Maze();
     boolean start = true;
+    public menus view = new mainmenu();
+    public  static MazeGenerator mazeGenerator = new MazeGenerator();
     public static Point2D end ;
 
     public static List<Point2D> wallsPositions = new ArrayList<>();
     public static Maze instance;
-
+    public static HashMap<Point2D, Node> playervalid = new HashMap<>();
     public static HashMap<Point2D, Node> valid = new HashMap<>();
     public void settings() {
         if (instance == null) {
             instance = this;
         }
-        size(800, 800);
+        size(1200, 800);
 
 
     }
     public void draw() {
         if (start) {
+            instance.ellipseMode(CENTER);
+
             start = false;
-            grid();
+         maze.view.view();
         }
+        if (maze.view.getMenu().equalsIgnoreCase("main")) {
+            gameView.circle();
+            gameView.highscore();
+
+        } else if (maze.view.getMenu().equalsIgnoreCase("menu")) {
+                mainmenu.text();
+        }
+
 
     }
     public void mousePressed() {
@@ -33,18 +51,15 @@ public class Maze extends PApplet{
         int y = mouseY;
         y = roundpos(y);
         x = roundpos(x);
-       // System.out.println(x + " dfd" + y);
-        MazeGenerator mazeGenerator = new MazeGenerator();
-        mazeGenerator.generate(new Point2D.Double(0,0));
-
-
-//        Point2D wall = new Point2D.Double(x,y);
-//       if (!wallsPositions.contains(addCord(x,y))) wall(wall);
+        maze.view.actions(x,y);
     }
 
     public void keyPressed() {
-        if (key == 'w') {
-            algorithm.shortest_path(addCord(0, 0), Maze.end, Maze.valid.get(Maze.end));
+        if (maze.view.getMenu().equalsIgnoreCase("main")) {
+
+            player.move(key);
+        } else if (maze.view.getMenu().equalsIgnoreCase("menu")) {
+            mainmenu.keyPressed(key);
         }
 
     }
@@ -55,33 +70,38 @@ public class Maze extends PApplet{
         return cord;
     }
 
-//    public void wall(Point2D wall) {
-//        wallsPositions.add(wall);
-//        valid.remove(wall);
-//        fill(0);
-//        rect((int)wall.getX(),(int)wall.getY(),25,25);
-//
-//    }
-
     public void grid() {
         for (int i = 0; i < 32; i++) {
             for (int j = 0; j < 32; j++) {
                 valid.put(addCord(j * 25, i * 25), new Node(j * 25, i * 25));
+                playervalid.put(addCord(j * 25, i * 25), new Node(j * 25, i * 25));
                 valid.get(addCord(j * 25, i * 25)).distance = MAX_INT;
-                fill(255,255,255);
-                rect(j * 25, i * 25, 25, 25);
-
-
+                Maze.instance.fill(255,255,255);
+                Maze.instance.rect(j * 25, i * 25, 25, 25);
             }
         }
 
-        fill(0,255,0);
-        rect(0, 0, 25, 25);
-
+        Maze.instance.fill(0,255,0);
+        Maze.instance.rect(0, 0, 25, 25);
     }
 
     public Point2D addCord(int xcord, int ycord) {
         return new Point2D.Double(xcord, ycord);
 
+    }
+      public void clearing() {
+
+        valid.clear();
+        gameView.fastest.clear();
+        wallsPositions.clear();
+        playervalid.clear();
+        MazeGenerator.movesvalid.clear();
+        player.x = 12.5f;
+        player.y = 12.5f;
+    }
+
+    public void reset() {
+        Maze.instance.fill(204 ,204, 204);
+        Maze.instance.rect(0,0,1200,800);
     }
 }
